20af9d167a247780e10f6f96c62c6f5725040f7b
Gokul Suresh <50659225+xenomech@users.noreply.github.com>
2022-12-25 23:24:04 -0800

    PD-05: add card image (#10)

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -11,7 +11,7 @@ export default function Container({ children }: Props) {
     keywords: "Pokemon | card | PokeCard | Nintendo 3DS | Pokemon x/y",
     type: "website",
     og_image: "/static/images/og_image.png",
-    twitter_image: "/static/images/Twitter_Card.png",
+    twitter_image: "/static/images/Twitter_Summary.png",
     twitterHandle: "@justgokulsuresh",
   };
   return (
@@ -26,10 +26,10 @@ export default function Container({ children }: Props) {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.og_image} />
         <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content={meta.twitterHandle} />
         <meta name="twitter:site" content={meta.twitterHandle} />
         <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:image" content={meta.twitter_image} />
       </Head>
       {children}
